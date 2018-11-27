ca584dbfe660f4c139398008a70a858017181255
Kyoseung Koo <grammaright@gmail.com>
2018-11-27 20:42:09 +0900

    (Only) Convert to Disk-based code from SqlRecord base code

--- src/kr/ac/snu/dbs/koo/JoinProcessor/JoinModule/SortMergeJoin.java
+++ src/kr/ac/snu/dbs/koo/JoinProcessor/JoinModule/SortMergeJoin.java
@@ -8,9 +8,7 @@ import kr.ac.snu.dbs.koo.SqlProcessor.TableElement.SqlRecord;
 import kr.ac.snu.dbs.koo.SqlProcessor.TableElement.SqlTable;
 import kr.ac.snu.dbs.koo.SqlProcessor.TableElement.SqlValue;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.ArrayList;
 
 public class SortMergeJoin {
@@ -46,8 +44,6 @@ public class SortMergeJoin {
             table1 = MergeSort.orderTable(table1, table1OrderList);
             table2 = MergeSort.orderTable(table2, table2OrderList);
 
-
-
             int target1ColumnIndex = matchTargetIndex(table1, joinCondition, table1.column);
             int target2ColumnIndex = matchTargetIndex(table2, joinCondition, table2.column);
 
@@ -56,49 +52,72 @@ public class SortMergeJoin {
                 throw new Exception("Error Condition: No attribute in table");
             }
 
-            // TODO: 이것도 Disk Based로..?
+            FileReader trFr = new FileReader(table2.tablePath);
+            FileReader gsFr = new FileReader(table1.tablePath);
+            FileReader tsFr = null;
+
+            BufferedReader trBr = new BufferedReader(trFr);
+            BufferedReader gsBr = new BufferedReader(gsFr);
+            BufferedReader tsBr = null;
+
             int tr = 0;     // ranges over R
             int ts = 0;     // ranges over S
             int gs = 0;     // start of current S-partition
-            while (tr != table2.records.size() && gs != table1.records.size()) {
-                SqlValue tri = table2.records.get(tr).values.get(target2ColumnIndex);
-                SqlValue gsj = table1.records.get(gs).values.get(target1ColumnIndex);
-                while (SqlValue.compare(tri, gsj) == 1) {
-                    if (++tr >= table2.records.size()) break;
-                    tri = table2.records.get(tr).values.get(target2ColumnIndex);      // continue scan of R
+
+            String trLine = null;
+            String tsLine = null;
+            String gsLine = null;
+            while (((trLine = trBr.readLine()) != null) && ((gsLine = gsBr.readLine()) != null)) {
+                SqlRecord tri = SqlRecord.constructRecord(null, trLine.split(" "));
+                SqlRecord gsj = SqlRecord.constructRecord(null, gsLine.split(" "));
+                while (SqlValue.compare(tri.values.get(target1ColumnIndex), gsj.values.get(target2ColumnIndex)) == 1) {
+                    if ((trLine = trBr.readLine()) == null) break;
+                    tri = SqlRecord.constructRecord(null, trLine.split(" "));
+                    tr++;   // continue scan of R
                 }
 
-                while (SqlValue.compare(tri, gsj) == -1) {
-                    if (++gs >= table1.records.size()) break;
-                    gsj = table1.records.get(gs).values.get(target1ColumnIndex);      // continue scan of S
+                while (SqlValue.compare(tri.values.get(target1ColumnIndex), gsj.values.get(target2ColumnIndex)) == -1) {
+                    if ((gsLine = gsBr.readLine()) == null) break;
+                    gsj = SqlRecord.constructRecord(null, gsLine.split(" "));
+                    gs++;   // continue scan of S
                 }
 
                 ts = gs;                                            // Needed in case Tri != Gsj
-                while (SqlValue.compare(tri, gsj) == 0) {           // process current R partition
+                while (SqlValue.compare(tri.values.get(target1ColumnIndex), gsj.values.get(target2ColumnIndex)) == 0) {           // process current R partition
                     ts = gs;                                        // reset S partition scan
-                    SqlValue tsj = table1.records.get(ts).values.get(target1ColumnIndex);
-                    while (SqlValue.compare(tsj, tri) == 0) {       // process current R tuple
-                        SqlRecord trRecord = table2.records.get(tr);
-                        SqlRecord tsRecord = table1.records.get(ts);
+                    tsBr.close();
+                    tsFr.close();
+                    tsFr = new FileReader(table1.tablePath);
+                    tsBr = new BufferedReader(tsFr);
+                    for (int i = 0; i < ts; i++) tsLine = tsBr.readLine();
+                    SqlRecord tsj = SqlRecord.constructRecord(null, tsLine.split(" "));
 
+                    while (SqlValue.compare(tsj.values.get(target2ColumnIndex), tri.values.get(target1ColumnIndex)) == 0) {       // process current R tuple
                         // write to disk
-                        for (int i = 0; i < trRecord.values.size(); i++) {
-                            bw.write(trRecord.values.get(i).toString() + " ");
+                        for (int i = 0; i < tri.values.size(); i++) {
+                            bw.write(tri.values.get(i).toString() + " ");
                         }
-                        for (int i = 0; i < tsRecord.values.size(); i++) {
-                            bw.write(tsRecord.values.get(i).toString() + " ");
+                        for (int i = 0; i < tsj.values.size(); i++) {
+                            bw.write(tsj.values.get(i).toString() + " ");
                         }
                         bw.write("\n");
 
-                        if (++ts >= table1.records.size()) break;
-                        tsj = table1.records.get(ts).values.get(target1ColumnIndex);
+                        if ((tsLine = tsBr.readLine()) == null) break;
+                        ts++;
+                        tsj = SqlRecord.constructRecord(null, tsLine.split(" "));
                     }
-                    if (++tr >= table2.records.size()) break;
-                    tri = table2.records.get(tr).values.get(target2ColumnIndex);
+
+                    if ((trLine = trBr.readLine()) == null) break;
+                    tr++;
+                    tri = SqlRecord.constructRecord(null, trLine.split(" "));
                 }
                 gs = ts;
-                if (gs >= table1.records.size()) break;
-                gsj = table1.records.get(gs).values.get(target1ColumnIndex);
+
+                gsBr.close();
+                gsFr.close();
+                gsFr = new FileReader(table1.tablePath);
+                gsBr = new BufferedReader(gsFr);
+                for (int i = 0; i < gs; i++) gsBr.readLine();
             }
 
             bw.close();
